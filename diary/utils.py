73541e4fccd75abd029cfacd17bf8f02b4c2bb37
Jelle de Jong <jelle@hoest.nl>
2013-02-13 16:40:36 +0100

    Added generate_thumb
    pip install pil

--- diary/utils.py
+++ diary/utils.py
@@ -3,6 +3,12 @@ from urlparse import urlparse, urljoin
 from flask import request
 import re
 import translitcodec
+import os
+
+try:
+  from PIL import Image
+except:
+  import Image
 
 _punct_re = re.compile(r"[\t !\"#$%&'()*\-/<=>?@\[\\\]^_`{|},.]+")
 
@@ -40,3 +46,43 @@ def get_redirect_target():
 def allowed_file(filename):
   return '.' in filename and \
     filename.rsplit('.', 1)[1] in app.config["ALLOWED_EXTENSIONS"]
+
+
+def generate_thumb(source, target, box, fit=True):
+  """
+  Downsample the image.
+
+  @param source: string - path to the image
+  @param target: string - path to the save the thumbnail-image
+  @param box:    tuple(x, y) - the bounding box of the result image
+  @param fit:    boolean - crop the image to fill the box
+  """
+  if source:
+    img = Image.open(source)
+
+    # preresize image with factor 2, 4, 8 and fast algorithm
+    factor = 1
+    while img.size[0] / factor > 2 * box[0] and img.size[1] * 2 / factor > 2 * box[1]:
+      factor *= 2
+    if factor > 1:
+      img.thumbnail((img.size[0] / factor, img.size[1] / factor), Image.NEAREST)
+
+    # calculate the cropping box and get the cropped part
+    if fit:
+      x1 = y1 = 0
+      x2, y2 = img.size
+      wRatio = 1.0 * x2 / box[0]
+      hRatio = 1.0 * y2 / box[1]
+      if hRatio > wRatio:
+        y1 = int(y2 / 2 - box[1] * wRatio / 2)
+        y2 = int(y2 / 2 + box[1] * wRatio / 2)
+      else:
+        x1 = int(x2 / 2 - box[0] * hRatio / 2)
+        x2 = int(x2 / 2 + box[0] * hRatio / 2)
+      img = img.crop((x1, y1, x2, y2))
+
+    # resize the image with best quality algorithm ANTI-ALIAS
+    img.thumbnail(box, Image.ANTIALIAS)
+    img.save(target)
+
+    return target
