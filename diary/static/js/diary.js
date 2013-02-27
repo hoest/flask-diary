8257ae05c4d84d13eace3c2a1d269754d135d505
Jelle de Jong <jelle@infoprojects.nl>
2013-02-27 14:15:10 +0100

    Enabled Aloha again

--- diary/static/js/diary.js
+++ diary/static/js/diary.js
@@ -1,46 +1,50 @@
-// var Aloha = window.Aloha || (window.Aloha = {});
-// Aloha.settings = {
-//   locale: 'nl',
-//   plugins: {
-//     format: {
-//       // all elements with no specific configuration get this configuration
-//       config: [ 'b', 'i', 'p' , 'removeFormat' ],
-//       sidebar: {
-//         disabled: false
-//       }
-//     }
-//   }
-// };
-
-$(function() {
-  post_id = getParameterByName('post_id');
-  if(post_id && post_id !== '') {
-    $('html, body').animate({
-      scrollTop: $('#post_' + post_id).offset().top
-    }, 500);
+var Aloha = window.Aloha || (window.Aloha = {});
+Aloha.settings = {
+  locale: 'nl',
+  plugins: {
+    format: {
+      // all elements with no specific configuration get this configuration
+      config : [  'b', 'i', 'p', 'sub', 'sup', 'h3', 'h4', 'h5', 'pre', 'removeFormat' ],
+      editables : {
+        // no formatting allowed for title
+        '#title' : [  ]
+      },
+      sidebar: {
+        disabled: true
+      }
+    }
   }
+};
 
-  $('.alert-block').delay(1000).slideUp();
+  $(function() {
+    post_id = getParameterByName('post_id');
+    if(post_id && post_id !== '') {
+      $('html, body').animate({
+        scrollTop: $('#post_' + post_id).offset().top
+      }, 500);
+    }
 
-  $('.edit-buttons li a').each(function() {
-    $(this).attr('title', $(this).text());
+    $('.alert-block').delay(1000).slideUp();
+
+    $('.edit-buttons li a').each(function() {
+      $(this).attr('title', $(this).text());
+    });
+
+    $('.delete-image').each(function() {
+      $(this).attr('title', $(this).text());
+    });
+
+    if (!Modernizr.inputtypes.date) {
+      $('#date').datepicker({ dateFormat: 'yy-mm-dd' });
+    }
+
+    Aloha.ready(function() {
+      var $ = Aloha.jQuery;
+      $('#body').aloha();
+    });
   });
 
-  $('.delete-image').each(function() {
-    $(this).attr('title', $(this).text());
-  });
-
-  if (!Modernizr.inputtypes.date) {
-    $('#date').datepicker({ dateFormat: 'yy-mm-dd' });
+  function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
   }
-
-  // Aloha.ready(function() {
-  //   var $ = Aloha.jQuery;
-  //   $('#body').aloha();
-  // });
-});
-
-function getParameterByName(name) {
-  var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
