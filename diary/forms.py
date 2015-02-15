a81c37c4f310817e589496bce5d5ce4b5d12d57b
Jelle de Jong <jelle@hoest.nl>
2015-02-15 14:26:00 +0000

    Update

--- diary/forms.py
+++ diary/forms.py
@@ -1,7 +1,8 @@
 from flask import url_for, redirect
-from flask.ext.wtf import HiddenField, Form, TextField, TextAreaField, \
-  PasswordField, Required, FileField, BooleanField
+from flask.ext.wtf import Form
 from flask.ext.wtf.html5 import EmailField, DateField
+from wtforms import HiddenField, TextField, TextAreaField, \
+  PasswordField, validators, FileField, BooleanField
 from utils import get_redirect_target, is_safe_url
 import datetime
 
@@ -10,24 +11,24 @@ class DiaryForm(Form):
   """
   Form for the Diary object
   """
-  title = TextField("Titel", validators=[Required()])
+  title = TextField("Titel", validators=[validators.DataRequired()])
 
 
 class PostForm(Form):
   """
   Form for the Post object
   """
-  title = TextField("Titel", validators=[Required()])
-  body = TextAreaField("Tekst", default="Voer hier je tekst in...", validators=[Required()])
-  date = DateField("Datum", validators=[Required()])
+  title = TextField("Titel", validators=[validators.DataRequired()])
+  body = TextAreaField("Tekst", default="Voer hier je tekst in...", validators=[validators.DataRequired()])
+  date = DateField("Datum", validators=[validators.DataRequired()])
 
 
 class PictureForm(Form):
   """
   Form for the Picture object
   """
-  title = TextField("Titel", validators=[Required()])
-  file = FileField("Afbeelding", validators=[Required()])
+  title = TextField("Titel", validators=[validators.DataRequired()])
+  file = FileField("Afbeelding", validators=[validators.DataRequired()])
 
 
 class RedirectForm(Form):
@@ -52,6 +53,6 @@ class LoginForm(RedirectForm):
   """
   Form for Login
   """
-  emailaddress = EmailField("Emailadres", validators=[Required()])
-  password = PasswordField("Wachtwoord", validators=[Required()])
+  emailaddress = EmailField("Emailadres", validators=[validators.DataRequired()])
+  password = PasswordField("Wachtwoord", validators=[validators.DataRequired()])
   remember_me = BooleanField("Blijf ingelogd", default=False)
